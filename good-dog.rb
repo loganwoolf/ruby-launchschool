94102e454f6fa333396525c00996c559b3f03778
Logan Woolf <loganwoolf@gmail.com>
2022-03-06 15:48:27 -0700

    Mixing in modules

--- good-dog.rb
+++ good-dog.rb
@@ -1,15 +1,35 @@
-class Animal
-  def initialize
+# Extract common methods to a superclass
+# Exceptions can be put in modules and mixed in
+
+# "is-a" relationship, class inheritance (only one super)
+# "has-a" relationship, interface inheritance (mixin)
+
+module Swimmable # convention is to use -able on verb modules 
+  def swim
+    puts "I'm swimming!"
   end
 end
 
-class Bear < Animal
-  def initialize(color)
-    super()
-    @color = color
-  end
+class Animal; end
+
+class Fish < Animal
+  include Swimmable # mixing in module
 end
 
-bear = Bear.new("black")
+class Mammal < Animal
+end
 
-p bear
+class Cat < Mammal
+end
+
+class Dog < Mammal
+  include Swimmable # mixing in module
+end
+
+sparky = Dog.new
+neemo  = Fish.new
+paws   = Cat.new
+
+sparky.swim
+neemo.swim
+# paws.swim # gives error
