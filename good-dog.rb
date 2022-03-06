465498b63a096361ceb02c0791b771e49d0fa425
Logan Woolf <loganwoolf@gmail.com>
2022-03-06 15:36:28 -0700

    Super in constructor with arguments

--- good-dog.rb
+++ good-dog.rb
@@ -14,4 +14,15 @@ class GoodDog < Animal
   end
 end
 
+class BadDog < Animal
+  def initialize(name, color)
+    super(name)
+    @color = color
+  end
+end
+
 bruno = GoodDog.new("brown")        # => #<GoodDog:0x007fb40b1e6718 @color="brown", @name="brown">
+turner = BadDog.new("Turner", "Grey")
+
+p bruno
+p turner
