31a3ef8e399efda9024cb190f08c22be8ee4e7a2
Logan Woolf <loganwoolf@gmail.com>
2022-03-06 14:13:21 -0700

    built in to_s method

--- good-dog.rb
+++ good-dog.rb
@@ -7,7 +7,17 @@ class GoodDog
     self.name = n
     self.age  = a * DOG_YEARS
   end
+
+  def to_s
+    # to_s is also called on every string interpolation #{}
+    "this dog's name is #{name} and its age is #{age} dog-years."
+  end
 end
 
 sparky = GoodDog.new("Sparky", 4)
-puts sparky.age             # => 28
+# #to_s belongs to every class
+# puts automatically calls to_s on its argument
+puts sparky             # => 28
+
+# p automatically calls inspect on its argument
+p sparky
