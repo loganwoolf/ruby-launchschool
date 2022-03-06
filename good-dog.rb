c52caf2a331290075e62f121c3c6016de217f47f
Logan Woolf <loganwoolf@gmail.com>
2022-03-06 15:27:32 -0700

    super in instance method

--- good-dog.rb
+++ good-dog.rb
@@ -1,33 +1,14 @@
 class Animal
   def speak
-    "#{self.name} speaks: Hello!" # Self refers to the instance
+    "Hello!"
   end
 end
 
-class GoodDog < Animal # Inherits methods from superclass Animal using '<'
-  attr_accessor :name
-
-  def initialize(n)
-    self.name = n
-  end
-
-  # Overrides the speak method in the super class
+class GoodDog < Animal
   def speak
-    "#{self.name} says 'Arf!'"
+    super + " from GoodDog class"
   end
 end
 
-class Cat < Animal
-  attr_accessor :name
-
-  def initialize(n)
-    self.name = n
-  end
-
-  # Speak method is inherited from superclass Animal
-end
-
-sparky = GoodDog.new("Rex")
-paws = Cat.new("Goop")
-puts sparky.speak           # => Hello!
-puts paws.speak             # => Hello!
+sparky = GoodDog.new
+puts sparky.speak # => "Hello! from GoodDog class"
